f055927be654daba70fc8cbf2268cb9d8da2bd0e
ARJUNIYYAPPAN <arjunarun083@gmail.com>
2025-11-03 20:50:08 +0530

    color tehme

--- frontend/src/pages/services/international/Verification.jsx
+++ frontend/src/pages/services/international/Verification.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Verification.css'; // Assuming you have a CSS file for styling
+import '../domestic/Verification.css'; // Assuming you have a CSS file for styling
 
 const Verification = () => {
     return (
@@ -7,11 +7,11 @@ const Verification = () => {
         <h1 className="verification-title">Verification Builds Trust</h1>
         <section className="verification-section">
         <h6 className="verification-subtitle">VERIFYING EMPLOYMENT HISTORY</h6>
-        <p className="verification-paragraph">
+        <p className="verification-text">
             Employment verification reports are essential to determine whether or not the applicant actually has the experience they claim to have. Virtually anyone can write a job on a resume, and it is your right as an employer to make sure that the job, along with job descriptions are truthful.
         </p>
         <h6 className="verification-subtitle">WHAT INFORMATION CAN I EXPECT?</h6>
-        <p className="verification-paragraph">
+        <p className="verification-text">
             Our comprehensive employment history reports will detail the following information:
         </p>
         <ul className="verification-list">
@@ -21,7 +21,7 @@ const Verification = () => {
             <li>Title held</li>
         </ul>
         <h6 className="verification-subtitle">BENEFITS OF EMPLOYMENT VERIFICATION</h6>
-        <p className="verification-paragraph">Employment verification reports allow potential employers to:</p>
+        <p className="verification-text">Employment verification reports allow potential employers to:</p>
         <ul className="verification-list">
             <li>Analyze any inconsistencies within the resume</li>
             <li>Ensure no misleading information was provided</li>
@@ -30,15 +30,15 @@ const Verification = () => {
             <li>Obtain peace of mind about the hiring decision</li>
         </ul>
         <h6 className="verification-subtitle">WE’RE HERE TO HELP</h6>
-        <p className="verification-paragraph">
+        <p className="verification-text">
             At Verification Services Inc. our motto is “we’re here to help,” and we take that seriously. Our goal is to ensure that when you hire a new employee, you have the most accurate and complete information about that person. This enables you to reduce your risks of liability, lower your employee turn-over rate, and ultimately make the best decision for you.
         </p>
         <h6 className="verification-title">VERIFYING EDUCATION HISTORY</h6>
-        <p className="verification-paragraph">
+        <p className="verification-text">
             Verifying an applicant’s education history is a crucial step in vetting an employee. LinkedIn reported, that in 2016, about 8% of applicants stated they had a degree on their job applications that they did not have. The best way to safeguard your company from hiring inexperienced and undereducated employees is by performing a verification of education report.
         </p>
         <h6 className="verification-subtitle">WHAT CAN BE FOUND WHEN VERIFYING A POTENTIAL EMPLOYEE’S EDUCATION HISTORY?</h6>
-        <p className="verification-paragraph">
+        <p className="verification-text">
             Our team of specialists can obtain different levels of education completed, either directly from the institution or from a third party verifier. Education verifications include:
         </p>
         <ul className="verification-list">
@@ -48,7 +48,7 @@ const Verification = () => {
             <li>Graduation status / degree earned</li>
         </ul>
         <h6 className="verification-subtitle">WHY IS VERIFYING EDUCATION NECESSARY?</h6>
-        <p className="verification-paragraph">
+        <p className="verification-text">
             Hiring an applicant that is not qualified for the job can result in major losses for a company. If not screened or tested properly, an unqualified employee can be detrimental to that business. The workload can prove to be unmanageable for that individual, resulting in poor performance. Our expert analysts confirm all degrees and diplomas earned by the applicant to ensure hiring managers only grant jobs an applicant is qualified to handle.
         </p>
         </section>
