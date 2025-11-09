a83255ef7444346b6882b117c5f9347e432cbe1a
ARJUNIYYAPPAN <arjunarun083@gmail.com>
2025-11-09 12:30:50 +0530

    Under maintenance

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
+const isMaintenance = process.env.REACT_APP_MAINTENANCE_MODE === 'true';
+
+if (isMaintenance) {
+  window.location.href = '/maintenance.html';
+} else {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
